681961eec277f363015f53a31e3fdc5c31514667
Oak Table <coder@hexagonalmagmacube.org>
2021-07-05 19:02:48 -0700

    Add random chance that TNT will explode

--- src/main/java/org/hexagonalmagmacube/longfusetnt/ExplosionPrimeEventListener.java
+++ src/main/java/org/hexagonalmagmacube/longfusetnt/ExplosionPrimeEventListener.java
@@ -1,5 +1,7 @@
 package org.hexagonalmagmacube.longfusetnt;
 
+import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.TNTPrimed;
@@ -9,9 +11,15 @@ import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 public class ExplosionPrimeEventListener implements Listener {
 
+    private final Random rand = new Random();
+
     @EventHandler
     public void onEvent(ExplosionPrimeEvent event) {
         Entity original = event.getEntity();
+        if (rand.nextInt(15) == 0) {
+            Bukkit.getLogger().info(original.getEntityId() + " will explode now.");
+            return;
+        }
         event.setCancelled(true);
 
         TNTPrimed newTNT = original.getWorld().spawn(original.getLocation(), TNTPrimed.class);
